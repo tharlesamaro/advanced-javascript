555bdb3db5aab9b941de3737632f5667779ef6d9
tharlesamaro <tharlesamaro@gmail.com>
2018-03-12 11:24:42 -0300

    Adding exercises.

---
+++ js/config.js
@@ -0,0 +1,10 @@
+function setConfig() {
+    var texts = {
+        "title" : "Shooping Control"
+    };
+
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+setConfig();
